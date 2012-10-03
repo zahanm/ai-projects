73102e709498dcc7f107ce2a14ac0bc1d2f59a7f
Zahan Malkani <zahanm@gmail.com>
2012-10-03 11:03:26 -0700

    buy fruits costs

--- 0tutorial/buyLotsOfFruit.py
+++ 0tutorial/buyLotsOfFruit.py
@@ -10,7 +10,7 @@
 To run this script, type
 
   python buyLotsOfFruit.py
-  
+
 Once you have correctly implemented the buyLotsOfFruit function,
 the script should produce the output:
 
@@ -23,14 +23,12 @@ fruitPrices = {'apples':2.00, 'oranges': 1.50, 'pears': 1.75,
 def buyLotsOfFruit(orderList):
     """
         orderList: List of (fruit, numPounds) tuples
-            
+
     Returns cost of order
-    """ 
-    totalCost = 0.0             
-    "*** YOUR CODE HERE ***"
-    return totalCost
-    
-# Main Method    
+    """
+    return sum(map(lambda x: fruitPrices[x[0]] * x[1], orderList))
+
+# Main Method
 if __name__ == '__main__':
     "This code runs when you invoke the script from the command line"
     orderList = [ ('apples', 2.0), ('pears', 3.0), ('limes', 4.0) ]
